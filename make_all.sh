662fd6ae29aa5bb91edfa983b7f0e89ce04ba236
hodsdon <chrischodsdon@gmail.com>
2023-02-17 15:49:04 -0500

    add make_all.sh options

--- make_all.sh
+++ make_all.sh
@@ -3,6 +3,18 @@ if [[ $* == *--eRPC* ]]; then
     rm eRPC/CMakeCache.txt
     rm eRPC/CMakeFiles -fr
 fi
+if [[ $* == *--clean* ]]; then
+cd proxy; make clean; cd ..
+cd client; make clean; cd ..
+cd sequencer; make clean; cd ..
+cd server; make clean; cd ..
+fi
+if [[ $* == *--touch* ]]; then
+cd proxy; touch proxy.cc; cd ..
+cd client; touch client.cc; cd ..
+cd sequencer; touch sequencer.cc; cd ..
+cd server; touch server.cc; cd ..
+fi
 cd proxy; make; cd ..
 cd client; make; cd ..
 cd sequencer; make; cd ..
