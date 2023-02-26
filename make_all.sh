6cf8599af5cb8e52f5e351d40fa1dc99abf303b5
hodsdon <chrischodsdon@gmail.com>
2023-02-25 20:27:13 -0500

    add --clean option to make_all.sh

--- make_all.sh
+++ make_all.sh
@@ -3,7 +3,9 @@ if [[ $* == *--eRPC* ]]; then
     rm eRPC/CMakeCache.txt
     rm eRPC/CMakeFiles -fr
 fi
+if [[ $* == *--clean* ]]; then
+    cd proxy; make clean; make; cd ..
+    cd client; make clean; make; cd ..
+fi
 cd proxy; make; cd ..
 cd client; make; cd ..
-# cd sequencer; make; cd ..
-# cd server; make; cd ..
