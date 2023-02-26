6a575ac2470ddfdb521d6ee5a42c6173d0a5315b
hodsdon <chrischodsdon@gmail.com>
2023-02-25 20:28:11 -0500

    reformat make_all.sh

--- make_all.sh
+++ make_all.sh
@@ -4,16 +4,16 @@ if [[ $* == *--eRPC* ]]; then
     rm eRPC/CMakeFiles -fr
 fi
 if [[ $* == *--clean* ]]; then
-cd proxy; make clean; cd ..
-cd client; make clean; cd ..
-cd sequencer; make clean; cd ..
-cd server; make clean; cd ..
+    cd proxy; make clean; cd ..
+    cd client; make clean; cd ..
+    cd sequencer; make clean; cd ..
+    cd server; make clean; cd ..
 fi
 if [[ $* == *--touch* ]]; then
-cd proxy; touch proxy.cc; cd ..
-cd client; touch client.cc; cd ..
-cd sequencer; touch sequencer.cc; cd ..
-cd server; touch server.cc; cd ..
+    cd proxy; touch proxy.cc; cd ..
+    cd client; touch client.cc; cd ..
+    cd sequencer; touch sequencer.cc; cd ..
+    cd server; touch server.cc; cd ..
 fi
 cd proxy; make; cd ..
 cd client; make; cd ..
